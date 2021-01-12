8d3335a3162b45939a0026931ce31581577b300c
Ritesh Singh <67305933+Ritesh-B-Singh@users.noreply.github.com>
2021-01-12 13:58:11 +0530

    Codechef

---
+++ Colour_me_yellow.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+#define ll long long int
+using namespace std;
+
+void solve()
+{
+    ll n, r, g, b, ans = 0;
+    cin >> n >> r >> g >> b;
+    ll left = 0;
+    ll right = min(n, min(r, b));
+    while (left <= right)
+    {
+        ll mid = (left + right) / 2;
+        ll k = (r - mid) + (b - mid) + g;
+        if (k >= mid)
+        {
+            ans = mid;
+            left = mid + 1;
+        }
+        else
+        {
+            right = mid - 1;
+        }
+    }
+    cout << ans << "\n";
+}
+
+int main()
+{
+    ll t, n, r, g, b;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
